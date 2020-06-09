349c081f53830bc78e1a2c08c18958fc460d72ab
Martín <martinalalu@gmail.com>
2020-06-09 13:49:56 -0300

    subo proyecto

---
+++ layers/layers.js
@@ -0,0 +1,47 @@
+var wms_layers = [];
+
+
+        var lyr_GCBA_0 = new ol.layer.Tile({
+            'title': 'GCBA',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://servicios.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/amba_con_transporte_3857@GoogleMapsCompatible/{z}/{x}/{-y}.png'
+            })
+        });
+var format_shp_1 = new ol.format.GeoJSON();
+var features_shp_1 = format_shp_1.readFeatures(json_shp_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_shp_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_shp_1.addFeatures(features_shp_1);
+var lyr_shp_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_shp_1, 
+                style: style_shp_1,
+                interactive: true,
+    title: 'shp<br />\
+    <img src="styles/legend/shp_1_0.png" /> 8 - 21<br />\
+    <img src="styles/legend/shp_1_1.png" /> 21 - 34<br />\
+    <img src="styles/legend/shp_1_2.png" /> 34 - 42<br />\
+    <img src="styles/legend/shp_1_3.png" /> 42 - 54<br />\
+    <img src="styles/legend/shp_1_4.png" /> 54 - 70<br />\
+    <img src="styles/legend/shp_1_5.png" /> 70 - 96<br />\
+    <img src="styles/legend/shp_1_6.png" /> 96 - 137<br />\
+    <img src="styles/legend/shp_1_7.png" /> 137 - 190<br />\
+    <img src="styles/legend/shp_1_8.png" /> 190 - 328<br />\
+    <img src="styles/legend/shp_1_9.png" /> 328 - 2613<br />'
+        });
+
+lyr_GCBA_0.setVisible(true);lyr_shp_1.setVisible(true);
+var layersList = [lyr_GCBA_0,lyr_shp_1];
+lyr_shp_1.set('fieldAliases', {'barrio': 'barrio', 'comuna': 'comuna', 'perimetro': 'perimetro', 'area': 'area', });
+lyr_shp_1.set('fieldImages', {'barrio': 'TextEdit', 'comuna': 'TextEdit', 'perimetro': 'TextEdit', 'area': 'TextEdit', });
+lyr_shp_1.set('fieldLabels', {'barrio': 'no label', 'comuna': 'inline label', 'perimetro': 'no label', 'area': 'no label', });
+lyr_shp_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/shp_1_style.js
@@ -0,0 +1,96 @@
+var size = 0;
+var placement = 'point';
+
+var style_shp_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("comuna");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 8.000000 && value <= 20.700000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(247,251,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 20.700000 && value <= 34.400000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(226,238,249,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 34.400000 && value <= 42.100000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(205,224,242,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 42.100000 && value <= 54.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(176,210,232,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 54.000000 && value <= 69.500000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(137,191,221,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 69.500000 && value <= 96.200000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(96,166,210,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 96.200000 && value <= 136.900000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(62,142,196,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 136.900000 && value <= 189.800000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(33,114,182,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 189.800000 && value <= 328.400000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(10,84,158,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 328.400000 && value <= 2613.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(8,48,107,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
